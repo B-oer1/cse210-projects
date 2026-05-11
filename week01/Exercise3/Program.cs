f89c942290a63790edff44a2b1c22bc416e6bca6
B-oer1 <boladele1@byupathway.edu>
2026-05-11 11:58:57 +0100

    guess completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,28 @@
 using System;
 
-class Program
+Console.WriteLine("Hello World! This is the Exercise3 Project.");
+Console.Write("What is the magic number? ");
+// int magicNumber = int.Parse(Console.ReadLine());
+
+Random randomGenerator = new Random();
+int magicNumber = randomGenerator.Next(1, 101);
+
+int guess = 0;
+do
 {
-    static void Main(string[] args)
+    Console.Write("What is your guess? ");
+    guess = int.Parse(Console.ReadLine());
+
+    if (magicNumber > guess)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Higher");
     }
-}
+    else if (magicNumber < guess)
+    {
+        Console.WriteLine("Lower");
+    }
+    else
+    {
+        Console.WriteLine("You guessed it, right!");
+    }
+} while (guess != magicNumber);
