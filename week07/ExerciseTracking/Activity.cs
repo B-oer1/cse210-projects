f88d874de65708f8610ef9222a5d8604f2a2d825
B-oer1 <boladele1@byupathway.edu>
2026-06-17 20:30:48 +0100

    Week 07 Excercise

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,28 @@
+using System;
+
+public abstract class Activity
+{
+    private DateTime _date;
+    private int _lengthInMinutes;
+
+    public Activity(DateTime date, int lengthInMinutes)
+    {
+        _date = date;
+        _lengthInMinutes = lengthInMinutes;
+    }
+
+    public DateTime Date => _date;
+    public int LengthInMinutes => _lengthInMinutes;
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{_date:dd MMM yyyy} {GetType().Name} ({_lengthInMinutes} min) - " +
+               $"Distance: {GetDistance():F1} miles, " +
+               $"Speed: {GetSpeed():F1} mph, " +
+               $"Pace: {GetPace():F2} min per mile";
+    }
+}
